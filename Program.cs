504c173dadd15d67df2fda023432a0c4bc774748
muhammadkodir <muhammadkodyra@email.com>
2022-11-17 21:00:13 +0500

    6

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0);
+{
+    Console.WriteLine("countable number");
+} 
+if (number % 2 ==1);
+{
+    Console.WriteLine("uncountable number");
+}
